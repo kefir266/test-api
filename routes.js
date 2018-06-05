a599309aabb60fb99eb94ff8d3001c6e8defcf69
Dmytro Horbatko <kefir266@yahoo.com>
2018-06-05 07:52:43 +0300

    Added CORS

--- routes.js
+++ routes.js
@@ -9,15 +9,22 @@ const bodyParser = require('body-parser');
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 
-router.post('/signin', contrUser.signIn);
-router.post('/signup', contrUser.signUp);
+router.post('/signin', setHeaders, contrUser.signIn);
+router.post('/signup', setHeaders, contrUser.signUp);
 router.post(
   '/logout',
   auth.isAuthenticated,
+  setHeaders,
   contrUser.logout);
 router.get(
   '/info',
   auth.isAuthenticated,
+  setHeaders,
   contrUser.info);
+
+function setHeaders(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+}
 
 module.exports = router;
